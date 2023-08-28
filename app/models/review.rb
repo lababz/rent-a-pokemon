4a05878b32de5d991f2a454948974467d62a8410
Hadrien G <hgrach@gmail.com>
2023-08-29 00:29:20 +0200

    seed: genère  10 users et 30 pokémons

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   # associations
-  belongs_to :booking,
-  belongs_to :user,
+  belongs_to :booking
+  belongs_to :user
 
   # validations
   validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+puts "Cleaning database..."
+User.destroy_all
+Pokemon.destroy_all
+
+puts "Creating users..."
+
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+
+puts "Creating pokemons..."
+
+pokemon_types = [
+  "Normal", "Fire", "Water", "Grass", "Electric", "Ice", "Fighting",
+  "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost",
+  "Dragon", "Dark", "Steel", "Fairy"
+]
+
+30.times do
+  Pokemon.create!(
+    name: Faker::Games::Pokemon.unique.name,
+    description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
+    pokemon_type: pokemon_types.sample,
+    location: Faker::Address.full_address,
+    user_id: User.all.sample.id
+  )
+end
+
+puts "Finished! 30 pokemons created!"
