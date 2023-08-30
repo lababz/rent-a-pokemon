7550624ebf040a418d1245b680de489510458c79
Dylan Cianci <cianci.dy@gmail.com>
2023-08-30 15:05:39 +0200

    changement des controllers et routes

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -31,10 +31,16 @@ class PokemonsController < ApplicationController
     redirect_to pokemons_path
   end
 
+  def update
+    @pokemon = Pokemon.find(params[:id])
+    @pokemon.update(pokemon_params)
+    redirect_to pokemon_path(@pokemon)
+  end
+
   private
 
   # Définit les paramètres autorisés pour le Pokémon
   def pokemon_params
-    params.require(:pokemon).permit(:name, :description, :pokemon_type, :location, images: [])
+    params.require(:pokemon).permit(:name, :description, :pokemon_type, :location, :price, images: [])
   end
 end
